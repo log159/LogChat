5682f9b4db40513cf270e1c0076fe6773e7da4bf
userName <userEmail>
2024-06-04 22:10:03 +0800

    更新 关于widget

--- LogGpt/aboutsoftwaredialogwidget.cpp
+++ LogGpt/aboutsoftwaredialogwidget.cpp
@@ -40,14 +40,27 @@ void AboutSoftwareDialogWidget::init()
                             "- UI优化，感谢GitHub@Zao-chen\n"
                             "- 使得设置信息可以保存\n"
                             "- 百度翻译处理不完全问题\n"
-                            "#7.3"
+                            "#7.3\n"
                             "- 添加了Galgame样式对话框\n"
+                            "- 右键点击l2d可出现对话框\n"
+                            "- 解决了Live2d设置滑动条两端非整数显示问题\n"
+                            "- 解决了消息界面大小突变时文本框的显示问题\n"
                             "#End 有Bug欢迎反馈！\n";
     QFont font;
     font.setPixelSize(20);
     ui->textEdit_about->setFont(font);
     ui->textEdit_about->setReadOnly(true);
     ui->textEdit_about->setText(information_str);
+
+    ui->textEdit_about->setStyleSheet(
+                "QScrollBar:vertical {width: 10px;background-color: #F5F5F5;margin: 0px 0px 0px 0px;border-radius: 5px;}"
+                "QScrollBar::handle:vertical {background-color: #CCCCCC;min-height: 20px;border-radius: 5px;}"
+                "QScrollBar::handle:vertical:hover {background-color: #BBBBBB;border-radius: 5px;}"
+                "QScrollBar::add-line:vertical {height: 0px; subcontrol-position: bottom; subcontrol-origin: margin;}"
+                "QScrollBar::sub-line:vertical {height: 0 px;subcontrol-position: top; subcontrol-origin: margin;}"
+                "QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {background-color: #F5F5F5;border-radius: 5px;}"
+                );
+
 
     /*读取qss文件*/
     QFile file(":/main.qss");
